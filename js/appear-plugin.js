203a54d22a60a514853ec24aafca039e62994331
unknown <marcin.adam.mroczko@gmail.com>
2018-08-22 22:30:40 +0200

    update js

--- js/appear-plugin.js
+++ js/appear-plugin.js
@@ -1,8 +1,9 @@
-window.sr = ScrollReveal({ mobile: false })
+// window.sr = ScrollReveal({ mobile: false })
 
 window.sr = ScrollReveal({ reset: true});
 
 sr.reveal('.about__txt-inner', {duration: 1500, origin: 'bottom'});
 sr.reveal('.avatar', {duration: 1500, origin: 'bottom'});
 sr.reveal('.about__interactive-box', {duration: 500, origin: 'bottom'});
+sr.reveal('.skills__container', {duration: 500, origin: 'bottom'});
 
