1bfd136e6974fbcfe738c3d6ebc473e4cd847565
unknown <marcin.adam.mroczko@gmail.com>
2018-08-30 09:51:13 +0200

    update apear plugin

--- js/appear-plugin.js
+++ js/appear-plugin.js
@@ -3,7 +3,8 @@
 window.sr = ScrollReveal({ reset: true});
 
 sr.reveal('.about__txt-inner', {duration: 1500, origin: 'bottom'});
-sr.reveal('.avatar', {duration: 1500, origin: 'bottom'});
+sr.reveal('#contact .bg-img__container-inner', {duration: 1500, origin: 'bottom'});
+sr.reveal('.avatar', {duration: 500, origin: 'bottom'});
 sr.reveal('.about__interactive-box', {duration: 500, origin: 'bottom'});
 sr.reveal('.skills__container', {duration: 500, origin: 'bottom'});
 
